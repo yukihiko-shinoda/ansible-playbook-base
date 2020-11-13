445bd9d90264320805a0e13482c3efbbba8ed9e8
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2020-11-14 04:07:18 +0900

    Add Ansible install script for container
    Since container shell can't run sudo.

---
+++ common-scripts/install_ansible_by_pip.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -eu
+# This code should be called after installing pyenv and default global Python.
+# @see https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#latest-releases-via-pip
+# @see https://developers.redhat.com/blog/2018/11/14/python-in-rhel-8/
+bash -lc 'pip install ansible pexpect'
